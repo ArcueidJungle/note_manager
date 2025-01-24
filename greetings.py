d42201f26fa22eb0bf939306eb5e8476ec850038
ArcueidJungle <kaminoonte00@gmail.com>
2025-01-24 12:18:31 +0300

    Grade 1. Этап 1: Задание 1

---
+++ greetings.py
@@ -0,0 +1,15 @@
+username = "Arcueid"
+title = "Student"
+content = "studying python"
+status = "Закрыта"
+created_date = "10-11-2024"
+issue_date = "10-12-2024"
+print("Имя пользователя: " + username)
+print("Заголовок заметки: " + title)
+print("Описание заметки: " + content)
+print("Cтатус заметки: " + status)
+print("Дата создания: " + created_date)
+print("Дата истечения заметки: " + issue_date )
+
+
+
